a31cfd79dc635eb88150ec7b56d6888d4c5a2d00
alexandre <alexandremr01@gmail.com>
2024-04-09 18:11:19 +0200

    add basic election logic

--- main.go
+++ main.go
@@ -14,9 +14,13 @@ import (
 type Server struct {
 	NodeID string 
 	LastHearbeat *time.Time
+	Clients map[string]*rpc.Client
+	ElectionHadResponse bool
 }
 
 type HearbeatArgs struct {Sender string}
+type ElectionArgs struct {Sender string}
+type RespondElectionArgs struct {Sender string}
 func (s *Server) SendHeartbeat(args *HearbeatArgs, reply *int64) error {
     log.Printf("Node %s: Received heartbeat from node %s\n", s.NodeID, args.Sender)
 	now := time.Now()
@@ -24,16 +28,54 @@ func (s *Server) SendHeartbeat(args *HearbeatArgs, reply *int64) error {
     return nil
 }
 
+func (s *Server) RespondElection(args *RespondElectionArgs, reply *int64) error {
+    log.Printf("Node %s: Received OK from node %s\n", s.NodeID, args.Sender)
+	s.ElectionHadResponse = true
+    return nil
+}
+
+func (s *Server) CallForElection(args *ElectionArgs, reply *int64) error {
+    log.Printf("Node %s: Received call for elections from node %s\n", s.NodeID, args.Sender)
+	client, ok := s.Clients[args.Sender]
+	if !ok {
+		log.Printf("Node %s: Node %s not connected\n", s.NodeID, args.Sender)
+		return nil
+	}
+	err := client.Call(
+		"Server.RespondElection", 
+		RespondElectionArgs{Sender: s.NodeID},
+		nil,
+	)
+	if err != nil {
+		log.Printf("Node %s: Error communicating with node %s: %s", s.NodeID, args.Sender, err)
+	}
+	// TODO: initiates an election
+    return nil
+}
+
 func main() {
 	ids := [3]string{"1", "2", "3"}
+	numIds := [3]int{1, 2, 3}
 
 	port := os.Getenv("PORT")
 	nodeID := os.Getenv("NODE_ID")
+	myID, err := strconv.Atoi(nodeID) 
+	if err != nil {
+		log.Fatal("error parsing node id:", err)
+	}
+
 	timeout, err := strconv.Atoi(os.Getenv("NODE_TIMEOUT"))
 	if err != nil {
 		log.Fatal("error parsing timeout:", err)
 	}
 	timeoutDuration := time.Duration(timeout)*time.Millisecond
+
+	electionTimeout, err := strconv.Atoi(os.Getenv("ELECTION_TIMEOUT"))
+	if err != nil {
+		log.Fatal("error parsing election timeout:", err)
+	}
+	electionDuration := time.Duration(electionTimeout)*time.Millisecond
+
 	heartbeatTime, err := strconv.Atoi(os.Getenv("HEARTBEAT_TIME"))
 	if err != nil {
 		log.Fatal("error parsing heartbeat time:", err)
@@ -42,10 +84,10 @@ func main() {
 
 
     log.Printf("My ID: %s\n", nodeID)
-	server := &Server{NodeID: nodeID}
+	clients := make(map[string]*rpc.Client)
+	server := &Server{NodeID: nodeID, Clients: clients}
 
 	go func() {
-		clients := make(map[string]*rpc.Client)
 		time.Sleep(heartbeatTimeDuration)
 		for _, id := range ids {
 			if id == nodeID {
@@ -78,10 +120,33 @@ func main() {
 			}
 		} else {
 			for {
+				time.Sleep(timeoutDuration)
 				if (server.LastHearbeat == nil) || (time.Now().Sub(*server.LastHearbeat) > timeoutDuration) {
 					log.Printf("Leader timed out")
-				}
-				time.Sleep(timeoutDuration)
+					server.ElectionHadResponse = false
+					for _, numID := range numIds {
+						id := strconv.Itoa(numID)
+					
+						if id == nodeID || numID < myID {
+							continue
+						}
+						err := clients[id].Call(
+							"Server.CallForElection", 
+							ElectionArgs{Sender: nodeID},
+							nil,
+						)
+						if err != nil {
+							log.Printf("Node %s: Error communicating with node %s: %s", nodeID, id, err)
+						}
+					}
+					time.Sleep(electionDuration)
+					if server.ElectionHadResponse {
+						log.Printf("Node %s: Election finished with responses, going back to normal.", nodeID)
+					} else {
+						// TODO: becomes leader
+						log.Printf("Node %s: Election finished without responses, becoming leader.", nodeID)
+					}
+				}			
 			}
 		}
     }()
