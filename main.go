01f7af32153f081fad1a45925bce6fdc69852b05
alexandre <alexandremr01@gmail.com>
2024-04-12 13:40:20 +0200

    remove unused function

--- main.go
+++ main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"flag"
-	"fmt"
 	"log"
 	"net"
 	"net/http"
@@ -50,40 +48,10 @@ func mainLoop(algorithm types.Algorithm, state *state.State, config *types.Confi
 		} else {
 			time.Sleep(config.TimeoutDuration)
 			if (state.LastHearbeat == nil) || (time.Now().Sub(*state.LastHearbeat) > config.TimeoutDuration) {
-				log.Printf("Leader timed out")
+				log.Printf("Node %s: Leader timed out", config.NodeID)
 				algorithm.StartElections()
 			}
 		}
-	}
-}
-
-type cliArguments struct {
-	configFile    string
-	algorithmName string
-	id            int
-	port          string
-}
-
-func parseCLI() *cliArguments {
-	// format algorithms list
-	algorithmList := algorithms.GetAlgorithmsList()
-	algorithmListStr := ""
-	for _, alg := range algorithmList {
-		algorithmListStr += alg + ","
-	}
-	algorithmListStr = algorithmListStr[:len(algorithmListStr)-1]
-	algorithmHelp := fmt.Sprintf("Algorithm name (%s)", algorithmListStr)
-	// get command line arguments
-	algorithmName := flag.String("algorithm", "raft", algorithmHelp)
-	configFile := flag.String("config", "config.json", "Configuration file")
-	port := flag.String("port", "8000", "Port to connect")
-	id := flag.Int("id", 1, "Node ID")
-	flag.Parse()
-	return &cliArguments{
-		configFile:    *configFile,
-		algorithmName: *algorithmName,
-		port:          *port,
-		id:            *id,
 	}
 }
 
